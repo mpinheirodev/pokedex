a2ae92055cdea269bd23e86e07efcd99ac4e15c2
Matheus Pinheiro <131805878+mpinheirodev@users.noreply.github.com>
2024-01-30 02:04:39 -0300

    Add pagina de detalhes pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,15 +8,17 @@ function loadMoreItems(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons) => {
     const newHtml = pokemons
       .map(
-        (pokemon) =>
-          `<li class="pokemon ${pokemon.type}">
+        (pokemon) => `
+      <a href="./poke-detail.html?pokemon=${
+        pokemon.number
+      }"><li class="pokemon ${pokemon.type}">
           <span class="number">#${pokemon.number}</span>
           <span class="name">${pokemon.name}</span>
 
           <div class="details">
             <ol class="types">
               ${pokemon.types
-                .map((type) => `<a href="./poke-detail.html?pokemon=${pokemon.number}"><li class="type ${type}">${type}</li></a>`)
+                .map((type) => `<li class="type ${type}">${type}</li>`)
                 .join("")}
             </ol>
 
@@ -25,8 +27,9 @@ function loadMoreItems(offset, limit) {
               alt="${pokemon.name}"
             />
           </div>
-        </li>`
-      ).join("")
+        </li></a>`
+      )
+      .join("");
     pokemonList.innerHTML += newHtml;
   });
 }
@@ -42,7 +45,7 @@ loadMoreBtn.addEventListener("click", () => {
     loadMoreItems(offset, newLimit);
 
     loadMoreBtn.parentElement.removeChild(loadMoreBtn);
-  }else {
+  } else {
     loadMoreItems(offset, limit);
   }
 });
--- assets/js/pokemon-detail.js
+++ assets/js/pokemon-detail.js
@@ -1,2 +1,64 @@
-
 let currentPokemonId = null;
+const pokemonDetail = document.getElementById("pokemonDetail");
+const pokemonContent = document.getElementById("pokemonContent");
+const pokeBody = document.getElementsByTagName("body")[0];
+
+// fazer novo fecth?
+// como pegar o id do pokemon?
+// utilizar o objeto já cosntruido?
+
+document.addEventListener("DOMContentLoaded", () => {
+  const pokemonId = new URLSearchParams(window.location.search).get("pokemon");
+
+  currentPokemonId = pokemonId;
+  getPokemon(pokemonId);
+});
+
+function getPokemon(pokemonId) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then(convertPokemon)
+    .then((pokemon) => {
+      const newHtmlDetail = `
+      <div class="header">
+        <a href="./index.html"><i class="fa-solid fa-arrow-left"></i></a>
+        <i class="fa-regular fa-heart"></i>
+      </div>
+
+      <section class="info">
+        <div class="detail">
+          <h2>${pokemon.name}</h2>
+          <span>#${pokemon.number}</span>
+        </div>
+        <ul class="types">
+        ${pokemon.types
+          .map((type) => `<li class="type ${type}">${type}</li>`)
+          .join("")}
+        </ul>
+        <img src="${pokemon.image}"
+        alt="${pokemon.name}" />
+      </section>`;
+      const newHtmlContent = `
+      <div class="about">
+        <div class="about-content">
+          <span>Altura</span>
+          <h3>${pokemon.altura / 10} m</h3>
+        </div>
+        <div class="about-content">
+          <span>Peso</span>
+          <h3>${pokemon.peso / 10} kg</h3>
+        </div>
+        <div class="about-content">
+          <span>Habilidades</span>
+          <div>
+          ${pokemon.habilidades.map((habilidade) => `<h3>${habilidade}</h3>`).join("")}
+          </div>
+        </div>
+      </div>`
+      pokeBody.classList.add(`${pokemon.type}`);
+      pokemonDetail.innerHTML += newHtmlDetail;
+      pokemonContent.innerHTML += newHtmlContent;
+    });
+}
